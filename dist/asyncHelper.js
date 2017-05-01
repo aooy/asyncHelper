2e7097649ace74ccec69b78534c80bc5d8078607
yangjingzhuo <yangjingzhuo@jiuan.com>
2017-05-01 23:38:39 +0800

    add call mode

--- dist/asyncHelper.js
+++ dist/asyncHelper.js
@@ -1,44 +1,58 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.async = factory());
+    (global.asyncHelper = factory());
 }(this, (function () { 'use strict';
 
 var browserWindow = typeof window !== 'undefined' ? window : undefined;
 var browserGlobal = browserWindow || {};
 var isNode = typeof self === 'undefined' && typeof process !== 'undefined' && ({}).toString.call(process) === '[object process]';
 var BrowserMutationObserver = browserGlobal.MutationObserver || browserGlobal.WebKitMutationObserver;
-var scheduleFlush = undefined;
+
 var isWorker = typeof Uint8ClampedArray !== 'undefined' && typeof importScripts !== 'undefined' && typeof MessageChannel !== 'undefined';
 
-function hasPromise(){
+var  hasPromise = (function () {
     var _this;
     if (typeof global !== 'undefined') {
         _this = global;
-    } else if (typeof this !== 'undefined') {
-        _this = this;
+    } else if (typeof self !== 'undefined') {
+        _this = self;
     } else {
         throw new Error ('global object is unavailable in this environment');
     }
-
+    var promiseToString = undefined;
     if(_this.Promise) {
         try {
-            promiseToString = Object.prototype.toString.call(P.resolve());
+            promiseToString = Object.prototype.toString.call(_this.Promise.resolve());
         } catch (e) {
-            // silently ignored
+
         }
     }
+    return promiseToString === '[object Promise]';
+})();
+
+
+var hasMicrotask = isNode || hasPromise || BrowserMutationObserver ? true : false;
+
+function toArray (likeArr) {
+    return Array.prototype.slice.call(likeArr);
 }
 
-function useNextTick() {
+function usePromise(fn) {
   return function () {
-    return process.nextTick(flush);
+    return Promise.resolve().then(fn);
   };
 }
 
-function useMutationObserver() {
+function useNextTick(fn) {
+  return function () {
+    return process.nextTick(fn);
+  };
+}
+
+function useMutationObserver(fn) {
   var iterations = 0;
-  var observer = new BrowserMutationObserver(flush);
+  var observer = new BrowserMutationObserver(fn);
   var node = document.createTextNode('');
   observer.observe(node, { characterData: true });
 
@@ -47,48 +61,114 @@ function useMutationObserver() {
   };
 }
 
-function useMessageChannel() {
+function useMessageChannel(fn) {
   var channel = new MessageChannel();
-  channel.port1.onmessage = flush;
+  channel.port1.onmessage = fn;
   return function () {
     return channel.port2.postMessage(0);
   };
 }
 
-function useSetTimeout() {
+function useSetTimeout(fn) {
   var globalSetTimeout = setTimeout;
   return function () {
-    return globalSetTimeout(flush, 1);
+    return globalSetTimeout(fn, 1);
   };
 }
 
-var queue = new Array(1000);
-function flush() {
-  for (var i = 0; i < len; i += 2) {
-    var callback = queue[i];
-    var arg = queue[i + 1];
+var scheduleFlush = undefined, taskMethod = undefined, mtaskMethod = undefined;
 
-    callback(arg);
+if(hasMicrotask) {
+    if (isNode) {
+      mtaskMethod = useNextTick;
+      scheduleFlush = useNextTick(flush);
+    } else if (hasPromise) {
+      mtaskMethod = usePromise; 
+      scheduleFlush = usePromise(flush);  
+    } else if (BrowserMutationObserver) {
+      mtaskMethod = useMutationObserver;  
+      scheduleFlush = useMutationObserver(flush);  
+    }
+} 
 
-    queue[i] = undefined;
-    queue[i + 1] = undefined;
-  }
-  len = 0;
-}
-
-var scheduleFlush = undefined;
-
-if (isNode) {
-  scheduleFlush = useNextTick();
-} else if (BrowserMutationObserver) {
-  scheduleFlush = useMutationObserver();
-} else if (isWorker) {
-  scheduleFlush = useMessageChannel();
+if (isWorker) {
+    taskMethod = useMessageChannel;
+    scheduleFlush = useMessageChannel(flush);
 } else {
-  scheduleFlush = useSetTimeout();
+    taskMethod = useSetTimeout;
+    scheduleFlush = useSetTimeout(flush);
 }
 
-return scheduleFlush;
+function flush () {
+    tasksQueue.length && runTasks(tasksQueue) && (tasksQueue.length = 0);
+}
 
+function runTasks (taskQueue) {
+    var taskLen = taskQueue.length;
+
+    for(var i = 0; i < taskLen; i++){
+        var fnStore = taskQueue[i];
+        var option = fnStore.option;
+        var fn = fnStore.fn;
+        var fnArgs = fnStore.argsQueue;
+        var type = fnStore.type;
+        var t;
+        if( (t = fnStore.option.callMode) !== undefined ){
+            if(t === 'first'){
+                callByType(type, fn, fnArgs[0], option.context);          
+                continue;
+            } else if (t === 'last') {
+                callByType(type, fn, fnArgs[fnArgs.length - 1], option.context);
+                continue;
+            }
+        }
+
+        for (var j = 0; j < fnArgs.length; j++) {
+            callByType(type, fn, fnArgs[j], option.context);     
+        }
+    }
+    return true;
+}
+
+function callByType (type, cb, args, context) {
+    if(type === 'TASK'){
+        taskMethod(function(){return cb.apply(context, args);})();
+    } else if (type === 'MTASK'){
+        cb.apply(context, args);
+    }
+}
+
+var tasksQueue = [];
+
+function mid (fn, type, option) {
+   var newfn = {
+         fn: fn,
+         type: type,
+         option: option || {},
+         argsQueue: []
+   };
+
+   tasksQueue.push(newfn)
+
+   return function () {
+       newfn.argsQueue.push(toArray(arguments));
+       scheduleFlush();
+   };
+}
+
+function task (fn, option) {
+    return mid(fn, 'TASK', option);
+}
+
+function mtask (fn, option) {
+    return  mid(fn, 'MTASK', option);
+}
+
+function asyncHelper () {
+   this.task = task;
+   this.mtask = mtask;
+}
+
+return new asyncHelper();
 
 })));
